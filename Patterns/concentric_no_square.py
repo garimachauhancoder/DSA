e92d8ea5e6f8b3b6423cd32752485b930a63ccf7
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-08-19 20:45:02 +0530

    concentric number square code

---
+++ Patterns/concentric_no_square.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the no. of lines: "))
+for i in range((2*n)-1):
+    for j in range((2*n)-1):
+        top=i
+        left=j
+        right=((2*n)-2) - j
+        down = ((2*n)-2) - i
+        print(n- min(min(top,down), min(left,right)), end=" ")
+    print()
