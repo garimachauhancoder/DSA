69328a33a08125acc67ea9110d395024d6cc9711
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-08-19 20:52:51 +0530

    alternate zero and one pattern

---
+++ Patterns/alternate_zero_one.py
@@ -0,0 +1,11 @@
+n = int(input("Enter number of lines:"))
+start=1
+for i in range(n):
+    if i%2==0:
+        start=1
+    else:
+        start=0
+    for j in range(i+1):
+        print(start, end=" ")
+        start=1-start
+    print()
