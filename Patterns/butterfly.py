fa2430e6f353e50768ffae6e8ce44caa3f18412d
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-08-20 10:28:20 +0530

    butterfly pattern code

---
+++ Patterns/butterfly.py
@@ -0,0 +1,18 @@
+n = int(input("Enter the no. of lines: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end =" ")
+    for j in range(2*(n-i-1)):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*", end =" ")
+    print()
+
+for i in range(n):
+    for j in range(n-i-1):
+        print("*", end=" ")
+    for j in range(2*(i+1)):
+        print(" ",end=" ")
+    for j in range(n-i-1):
+        print("*",end=" ")
+    print()
