260b81f4a91da31402f2b7729f414618df267164
Bhawana Chauhan <bhawanachauhan6451@gmail.com>
2026-08-26 13:07:02 +0530

    second largest approach 2

--- Arrays/Easy/second_largest.py
+++ Arrays/Easy/second_largest.py
@@ -1,14 +1,34 @@
-# If same nuumber are not present into the array then this app. would be successful.
+# If same nuumber are not present into the array then this approach 1 would be successful.
+# Approach 1:
+# n = int(input("Enter the no. of lines: "))
+# a=[]
+# for i in range(n):
+#     num = int(input(f"Element {i+1}: "))
+#     a.append(num)
+
+# largest = a[n-1]
+# second_largest = a[n-2]
+# for i in range(n-2,0):
+#     if a[i]!=largest:
+#         second_largest=a[i]
+#         break
+# print("Second largest element: ", second_largest)
+
+# Approach 2:
 n = int(input("Enter the no. of lines: "))
 a=[]
 for i in range(n):
-    num = int(input(f"Element {i+1}: "))
+    num=int(input(f"Element {i+1}: "))
     a.append(num)
 
-largest = a[n-1]
-second_largest = a[n-2]
-for i in range(n-2,0):
-    if a[i]!=largest:
-        second_largest=a[i]
+largest = a[0]
+for i in range(n):
+    if a[i]>largest:
+        largest=a[i]
+
+second_largest = -1
+for i in range(n):
+    if a[i]>second_largest and a[i]!=largest:
+        second_largest = a[i]
         break
-print("Second largest element: ", second_largest)
+print("Second largest element is: ", second_largest)
