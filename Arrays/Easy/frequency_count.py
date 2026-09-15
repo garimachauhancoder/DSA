1b07de2d066ae7c1d0dcb3e4969728be7afced9d
Garima Chauhan <garimachauhan2301@gmail.com>
2026-09-15 18:27:13 +0530

    Frequency count which element occurs 1 times or others 2 times

---
+++ Arrays/Easy/frequency_count.py
@@ -0,0 +1,13 @@
+n = int(input("Enter total no. of elements: "))
+a = []
+for i in range(n):
+    num = int(input(f"Element {i}:"))
+    a.append(num)
+
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if a[i]==a[j]:
+            count +=1
+    if count==1:
+        print(f"Count of {a[i]}: {count} times")
